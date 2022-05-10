70de86b56fee2a9e3fea8c992e71c8cc7084fa6d
nnmnn1022 <41253926+nnmnn1022@users.noreply.github.com>
2022-05-10 18:47:33 +0900

    0510_commit

--- csv_module.py
+++ csv_module.py
@@ -1,15 +1,30 @@
 import csv
 
 def write(data, path) :
-    with open(path, 'w', encoding='utf-8-sig', newline='') as writeFile:
-        try:
-            csvWriter = csv.writer(writeFile)
-            csvWriter.writerows(data)
-        except Exception as e:
-            print(e)
+    if "'" or '"' in path :
+        path = path.replace('"', '').replace("'", '')
+    if path.startswith('& ') :
+        path = path.replace('& ', '', 1)
+    file_path = f'{path}/setting.csv'
+    try :
+        with open(file_path, 'w', encoding='utf-8-sig', newline='') as writeFile:
+            try:
+                csvWriter = csv.writer(writeFile)
+                csvWriter.writerows(data)
+            except Exception as e:
+                print(e)
+    except Exception as e :
+        print(e)
 
 def read(path) :
-    with open(path, 'r', encoding='utf-8-sig') as csvFile:
-            csvReader = list(csv.reader(csvFile))
+    if "'" or '"' in path :
+        path = path.replace('"', '').replace("'", '')
+    if path.startswith('& ') :
+        path = path.replace('& ', '', 1)
+    try :
+        with open(path, 'r', encoding='utf-8-sig') as csvFile:
+                csvReader = list(csv.reader(csvFile))
+    except Exception as e :
+        print(e)
 
     return csvReader
--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-from . import csv_module, crawling
+import csv_module, crawling
 import pathlib, os
 from datetime import datetime
+import pandas as pd
 
-
-saved_folder = pathlib.Path.cwd()
+# 설정 파일이 있는지 확인
+saved_folder = str(pathlib.Path.cwd())
 file_list = os.listdir(saved_folder)
-setting = False
+myfolder = ''
 
+# 설정 파일이 없으면 설정 시킴
 if 'setting.csv' not in file_list :
-    myfolder = input('파일을 저장할 폴더 절대 경로를 입력하세요.\n')
+    while myfolder == '' :
+        myfolder = [[input('파일을 저장할 폴더 절대 경로를 입력하세요.\n')]]
     csv_module.write(myfolder, saved_folder)
 
-data = input('크롤링이 필요한 파일을 넣어주세요.(.csv)\n')
+# file = input('크롤링이 필요한 파일을 넣어주세요.(.csv)\n')
+file = "D:\\!Project\\Umoo\\umoo\\Wemade\\1_Work\\0504_0517_REGULAR\\0509_xlsx\\[BG_Mir4] Mini Glossary - 0517 update.csv"
+if "'" or '"' in file :
+    file = file.replace('"', '').replace("'", '')
+if file.startswith('& ') :
+        file = file.replace('& ', '', 1)
+
+data =[]
+word_list = pd.read_csv(file, header=0)
+# col_num = int(input('몇 번째 열을 불러올까요?\n'))
+col_num = 1
+col_info = word_list.columns[col_num - 1]
+# pandas dataframe 객체 - dataframe[컬럼] 열 조회 / dataframe.ix[인덱스] 행 조회
+for word in word_list[col_info] :
+    data.append(word)
+    data.extend(crawling.run(word))
+
+# 오늘 날짜 반환
+date = datetime.today().strftime('%m%d')
+
+# 아까 myfolder를 만들었으면 그걸 쓰고, 아니면 setting 파일을 찾아서 myfolder 위치 반환
+
+if myfolder == '' :
+    setting_file = csv_module.read(f'{saved_folder}/setting.csv')
+    file_path = f'{setting_file[0][0]}\\{date}_크롤링.txt'
+else :
+    file_path = f'{myfolder}/{date}_크롤링.txt'
+
+with open(file_path, 'w', encoding='utf-8-sig') as writeFile:
+    for d in data :
+        if d is not None :
+            writeFile.write(d + '\n')
